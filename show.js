5c0ff9d71d50d5924b43b33ab5eb5fdedbf0b4eb
Simon Socolow <simonsocolow78@gmail.com>
2020-07-27 15:59:40 +0000

    adding more songs

--- show.js
+++ show.js
@@ -3,6 +3,9 @@ function quill(n)
     document.getElementById("qs").style.display = "none";
     document.getElementById("ylml").style.display = "none";
     document.getElementById("abnf").style.display = "none";
+    document.getElementById("ed").style.display = "none";
+    document.getElementById("ff").style.display = "none";
+    document.getElementById("vlfb").style.display = "none";
 
     if(n == 1)
     {
@@ -15,5 +18,17 @@ function quill(n)
     if(n == 3)
     {
         document.getElementById("abnf").style.display = "block";
+    }
+    if(n == 4)
+    {
+        document.getElementById("ed").style.display = "block";
+    }
+    if(n == 5)
+    {
+        document.getElementById("ff").style.display = "block";
+    }
+    if(n == 6)
+    {
+        document.getElementById("vlfb").style.display = "block";
     }
 }
